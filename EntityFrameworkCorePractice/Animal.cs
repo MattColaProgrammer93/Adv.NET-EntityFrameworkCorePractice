882d5af23c728cacb35ae5734dd8dfdd6ed28376
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-20 14:14:33 -0700

    Created the Animal model to track in the database

---
+++ EntityFrameworkCorePractice/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCorePractice
+{
+    /// <summary>
+    /// A individual animal 
+    /// </summary>
+    public class Animal
+    {
+        [Key] // Tells the EF Core to make this a Primary Key (PK)
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
