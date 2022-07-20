48b4a2ee06d1c01ddc883f816879d524a5f25bec
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-20 14:06:41 -0700

    Configured DbContext class to use the SQL Server

--- EntityFrameworkCorePractice/AnimalContext.cs
+++ EntityFrameworkCorePractice/AnimalContext.cs
@@ -11,6 +11,17 @@ namespace EntityFrameworkCorePractice
     // https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
     internal class AnimalContext : DbContext
     {
+        public AnimalContext()
+        {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Database = The name for the database
+            // Server = The server we are connecting to. LocalDB is included with VS
+            // Trusted_Connection - indicates that our windows account should be used
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Matt-EFCorePractice;Trusted_Connection=True;");
+        }
     }
 }
